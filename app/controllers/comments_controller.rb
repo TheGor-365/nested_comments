01295dd158b2b894ed9cdf444f6e936212d1e20b
Gor <7495500@gmail.com>
2024-09-06 21:32:54 +0300

    Add reply form styles, add parent_id, add parent to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,8 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(
-      :content
+      :content,
+      :parent_id
     ).merge(post_id: params[:post_id])
   end
 end
